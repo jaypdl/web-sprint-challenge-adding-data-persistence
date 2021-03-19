fca3875c908b59a46d9250978e67ea4fb04ebb94
Jay Ponce de Leon <jayponcedeleon@gmail.com>
2021-03-19 09:15:57 -0600

    wip router empty skelleton

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,14 @@
 // build your `/api/projects` router here
+const router = require('express').Router()
+
+
+// Error catching
+router.use((err, req, res, next) => { // eslint-disable-line
+  res.status(500).json({
+    message: `Something went wrong with your ${req.method} request in the projects router`,
+    errMessage: err.message,
+    stack: err.stack
+  })
+})
+
+module.exports = router
--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,13 @@
 // build your `/api/tasks` router here
+const router = require('express').Router()
+
+// Error catching
+router.use((err, req, res, next) => { // eslint-disable-line
+  res.status(500).json({
+    message: `Something went wrong with your ${req.method} request in the tasks router`,
+    errMessage: err.message,
+    stack: err.stack
+  })
+})
+
+module.exports = router
